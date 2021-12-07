4d7772cf5736996b3f6f4bdf497f87ab4e7b11e5
Eros Orlando <erosorlando@gmail.com>
2021-12-07 21:31:43 +0100

    day 7.2

--- 2021/day7.c
+++ 2021/day7.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX (1000000)
-#define DAYS (80)
-
-int nArray=0;
+#define MAX (1000)
 
 char* replaceWord(const char* s, const char* oldW,
-                  const char* newW)
-{
+                  const char* newW){
     char* result;
     int i, cnt = 0;
     int newWlen = strlen(newW);
@@ -44,8 +40,7 @@ char* replaceWord(const char* s, const char* oldW,
     return result;
 }
 
-int split (const char *txt, char delim, char ***tokens)
-{
+int split (const char *txt, char delim, char ***tokens){
     int *tklen, *t, count = 1;
     char **arr, *p = (char *) txt;
 
@@ -68,56 +63,127 @@ int split (const char *txt, char delim, char ***tokens)
     return count;
 }
 
-unsigned long readInput(FILE* filePointer,short input[MAX][MAX]){
-   int bufferLength = 1000;
+int readInput(FILE* filePointer,int* input){
+   int bufferLength = 4000;
    char buffer[bufferLength];
    char **numbers;
    int i=0;
-   
+   int sum=0;
    fgets(buffer, bufferLength, filePointer);
-   unsigned long nFisches=split (buffer, ',', &numbers);
-   for (int i=0; i<nFisches; i++){
-	input[nArray][i]=atoi(numbers[i]);
+   int nPoints=split (buffer, ',', &numbers);
+   for (int i=0; i<nPoints; i++){
+	input[i]=atoi(numbers[i]);
+	sum+=input[i];
    }
    
-   return nFisches;
+   return sum;
 }
 
-void printInput(short input[MAX][MAX], unsigned long nFisches){
-	for (int i=0; i<nFisches; i++){
-		printf("pesce: %d\t",input[nArray][i]);
+void merge(int arr[], int l, int m, int r){
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+  
+    /* create temp arrays */
+    int L[n1], R[n2];
+  
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+  
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+  
+    /* Copy the remaining elements of L[], if there
+    are any */
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+  
+    /* Copy the remaining elements of R[], if there
+    are any */
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+  
+
+void mergeSort(int arr[], int l, int r){
+    if (l < r) {
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l + (r - l) / 2;
+  
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+  
+        merge(arr, l, m, r);
+    }
+}
+
+void printArray(int A[]){
+    int i;
+    for (i = 0; i < MAX; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+
+
+int fuelSpent(int* input, int median){
+	int fuel=0;
+	for(int i=0; i<MAX; i++)
+		fuel+=abs(input[i]-median);
+	
+	return fuel;
+}
+
+int fuelSpent2(int* input, int media){
+	int fuel=0;
+	for(int i=0; i<MAX; i++){
+		int val =abs(input[i]-media);
+		fuel+=(val*(val+1))/2;
 	}
+	
+	return fuel;
 }
-
-void countFishes(short input[MAX][MAX],unsigned long* nFisches){
-	int currentFishes=(*nFisches);
-	for (int i=0; i<currentFishes; i++) {
-		if(input[nArray][i]==-1) break;
-		if(input[nArray][i]==0){
-			input[nArray][i]=6;
-			input[nArray][(*nFisches)++]=8;
-		}
-		else input[nArray][i]--;
-	}
-}
-
 
 int main() {
-   short input[MAX][MAX]={-1};
-   printf("1");
+   int input[MAX]={-1};
    FILE* filePointer;
-   filePointer = fopen("input_day6", "r");
-   unsigned long nFisches=readInput(filePointer,input);
+   filePointer = fopen("input_day7", "r");
+   int sum=readInput(filePointer,input);
    fclose(filePointer); 
-   
+
+   mergeSort(input, 0, MAX-1);
+   int media=sum/MAX;
+   int median=((MAX+1)/2) -1;
+
+   int fuel = fuelSpent(input, input[median]);
+   int fuel2 = fuelSpent2(input, media);
    //printInput(input);
-   for (int d=0; d<DAYS; d++)
-   {
-	   printf("%d iter: %lu\n",d,nFisches);
-	   countFishes(input,&nFisches);
-   }
-   printf("prima stella: %lu\n",nFisches);
+
+   printf("prima stella: %d\n",fuel);
    
-   //printf("seconda stella: %d\n",output);
+   printf("seconda stella: %d\n",fuel2);
    return 0;
 }
